b217ee60723784b5ccf3101bd3d51c6a46b68445
Harsh Raj <harshraj8843@gmail.com>
2022-10-13 04:42:46 +0530

    try: update user invite workflow

--- action.js
+++ action.js
@@ -1,6 +1,7 @@
 import core from "@actions/core";
 
 import addAllToTeam from "./scripts/addAllToTeam.js";
+import inviteUser from "./scripts/inviteUser.js";
 
 // main action function
 (async () => {
@@ -9,14 +10,22 @@ import addAllToTeam from "./scripts/addAllToTeam.js";
 
     // get action default data
     const OWNER = await core.getInput("OWNER");
-    // const REPO = await core.getInput("REPO");
+    const REPO = await core.getInput("REPO");
 
     const TOKEN = await core.getInput("TOKEN");
 
     const ADD_ALL_TO_TEAM = await core.getInput("ADD_ALL_TO_TEAM");
 
+    const INVITE_USER = await core.getInput("INVITE_USER");
+    const USERNAME = await core.getInput("USERNAME");
+    const ISSUE_NUMBER = await core.getInput("ISSUE_NUMBER");
+
     if (ADD_ALL_TO_TEAM === "true") {
       await addAllToTeam(OWNER, TOKEN);
+    }
+
+    if (INVITE_USER === "true") {
+      await inviteUser(OWNER, REPO, TOKEN, USERNAME, ISSUE_NUMBER);
     }
 
     // end of main function
