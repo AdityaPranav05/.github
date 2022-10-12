352ad62d447943f9c3054e2ce195b80312979c65
Harsh Raj <harshraj8843@gmail.com>
2022-10-13 03:45:57 +0530

    update invite workflow

--- scripts/addAllToTeam.js
+++ scripts/addAllToTeam.js
@@ -1,18 +1,40 @@
 import fetch from "node-fetch";
 
+import sleep from "./sleep.js";
+
 export default async function addAllToTeam(OWNER, TOKEN) {
   await console.log("Adding all to team");
 
-  // get all team members
-  const teamMembers = await fetch(`https://api.codinasion.org/members`).then(
-    (res) => res.json()
-  );
+  // // get all team members
+  // const teamMembers = await fetch(`https://api.codinasion.org/members`).then(
+  //   (res) => res.json()
+  // );
 
-  // invite all members to the team
-  for (const member of teamMembers) {
-    await console.log(`Inviting ${member.username} to the team :)`);
+  // // invite all members to the team
+  // for (const member of teamMembers) {
+  //   await console.log(`Inviting ${member.username} to the team :)`);
+  //   await fetch(
+  //     `https://api.github.com/orgs/${OWNER}/teams/everyone/memberships/${member.username}`,
+  //     {
+  //       method: "PUT",
+  //       headers: {
+  //         Authorization: `token ${TOKEN}`,
+  //         Accept: "application/vnd.github.v3+json",
+  //       },
+  //     }
+  //   );
+  // }
+
+  // get all contributors
+  const contributors = await fetch(
+    `https://api.codinasion.org/contributors`
+  ).then((res) => res.json());
+
+  // invite all contributors to the team
+  for (const contributor of contributors) {
+    await console.log(`Inviting ${contributor.username} to the team :)`);
     await fetch(
-      `https://api.github.com/orgs/${OWNER}/teams/everyone/memberships/${member.username}`,
+      `https://api.github.com/orgs/${OWNER}/teams/everyone/memberships/${contributor.username}`,
       {
         method: "PUT",
         headers: {
@@ -21,5 +43,9 @@ export default async function addAllToTeam(OWNER, TOKEN) {
         },
       }
     );
+
+    // wait for 2 second
+    // to prevent github api secondary rate limit
+    await sleep(2000);
   }
 }
